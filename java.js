d5ee8feb14f5b6b0c2e44f34c69c3186fdbe3395
Zentnik <111569372+Zentnik@users.noreply.github.com>
2024-06-18 14:20:08 +0200

    Add files via upload

--- java.js
+++ java.js
@@ -1,7 +1,19 @@
 let click = document.getElementById('text');
 let coin = document.getElementById('button');
-let counter = 0;
-coin.addEventListener('click', function(){
+let counter = loadCounter();
+
+click.textContent = 'Гречки продано: ' + counter;
+
+coin.addEventListener('click', function() {
     counter++;
-    click.textContent='Гречки продано: '+counter;
+    click.textContent = 'Гречки продано: ' + counter;
+    saveCounter(counter);
 });
+
+function saveCounter(count) {
+    localStorage.setItem('Clicks', count);
+}
+
+function loadCounter() {
+    return parseInt(localStorage.getItem('Clicks')) || 0;
+}
